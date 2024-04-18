0b5c639939a6ab238a692adbfc6505b21b618bde
Abdullah Saleem <abdullahwork321@gmail.com>
2024-04-18 09:04:59 -0700

    added index.ts

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of these operators for start operation",
+        type: "list",
+        name: "operators",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+
+if (answer.operators === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operators === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operators === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operators === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+} else {
+    console.log("please select one of the operators");
+}
